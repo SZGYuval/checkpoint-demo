a5fe74ef90ba907d5dea13f80b90a734cfb4af9b
SZGYuval <yuval100r@gmail.com>
2025-08-14 10:20:49 +0300

    files

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-resource "aws_ssm_parameter" "foo" {
-  name = "foo"
-  type = "String"
-  value = "bar"
-}
-resource "aws_ssm_parameter" "check" {
-  name = "check"
-  type = "String"
-  value = "test"
-}
-resource "aws_ssm_parameter" "second" {
-  name = "second"
-  type = "String"
-  value = "second"
-}
-
 module "ecrRepo" {
   source = "./modules/ecr"
   ecr_repo_name = local.ecr_repo_name
